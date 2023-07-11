42f2e795d940ac7269b1b624cdd42908d8ae31ed
Mark Jensen <marknjensen@outlook.com>
2023-07-12 01:38:05 +0200

    it works

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,52 @@
+use std::collections::HashMap;
 
 use csv_types::*;
 
 fn read_file() {
-    let toggle_entries = csv::Reader::from_path("data/Toggl_Track_summary_report_2023-07-11_2023-07-11.csv").unwrap()
-    .into_deserialize()
-    .collect::<Result<Vec<ToggleTimeCSVEntry>, _>>().expect("Error reading toggle summary report");
+    let toggle_entries =
+        csv::Reader::from_path("data/Toggl_Track_summary_report_2023-07-11_2023-07-11.csv")
+            .unwrap()
+            .into_deserialize()
+            .collect::<Result<Vec<ToggleTimeCSVEntry>, _>>()
+            .expect("Error reading toggle summary report");
+
+    let mapping_entries = csv::Reader::from_path("data/mapping.csv")
+        .unwrap()
+        .into_deserialize()
+        .collect::<Result<Vec<MappingCSVEntry>, _>>()
+        .expect("Error reading mapping");
+
+    let mapping_entries_map = mapping_entries
+        .into_iter()
+        .map(|entry| (TaskKey::from(&entry), entry.tidsreg_path))
+        .collect::<HashMap<_, _>>();
+
+    let mut mapped_outputs = Vec::new();
+    let mut unmapped_outputs = Vec::new();
+    for toggle_entry in toggle_entries {
+        let path_option = mapping_entries_map.get(&TaskKey::from(&toggle_entry));
+
+        match path_option {
+            Some(path) => mapped_outputs.push(Output {
+                description: toggle_entry.description,
+                tidsreg_path: path.clone(),
+                duration: toggle_entry.duration,
+            }),
+            None => unmapped_outputs.push(toggle_entry),
+        }
+    }
+
+    mapped_outputs.sort_by_key(|e| e.tidsreg_path.clone());
 
 
-    let mapping_entries = csv::Reader::from_path("data/mapping.csv").unwrap()
-    .into_deserialize()
-    .collect::<Result<Vec<MappingCSVEntry>, _>>().expect("Error reading mapping");
+    println!("====================== Sucessfully mapped ======================");
+    println!("{mapped_outputs:#?}");
 
-    print!("{toggle_entries:#?}");
-    print!("{mapping_entries:#?}");
+    println!("====================== Add mapping data ======================");
+    println!("{unmapped_outputs:#?}");
 }
 
-fn main()  {
+fn main() {
     read_file()
 }
 
@@ -43,4 +74,39 @@ mod csv_types {
         #[serde(rename = "TidsregPath")]
         pub tidsreg_path: String,
     }
+}
+
+#[derive(Hash, Eq, PartialEq, Debug)]
+struct TaskKey {
+    pub project: String,
+    pub client: String,
+    pub description: String,
+}
+
+impl From<&ToggleTimeCSVEntry> for TaskKey {
+    fn from(value: &ToggleTimeCSVEntry) -> Self {
+        Self {
+            project: value.project.clone(),
+            client: value.client.clone(),
+            description: value.description.clone(),
+        }
+    }
+}
+
+impl From<&MappingCSVEntry> for TaskKey {
+    fn from(value: &MappingCSVEntry) -> Self {
+        Self {
+            project: value.project.clone(),
+            client: value.client.clone(),
+            description: value.description.clone(),
+        }
+    }
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+struct Output {
+    pub description: String,
+    pub tidsreg_path: String,
+    pub duration: String,
 }
